4b2434111545b43125068f0042cdf208661b5a8e
Julie Rindone <julie.rindone@gmail.com>
2026-03-30 00:19:12 -0700

    Added prices and backdrop filter effect to crypto section

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const photographer = document.getElementById('author');
 const cryptoDiv = document.getElementById('crypto');
+const cryptoTop = document.getElementById('crypto-top');
 
 getBackgroundPic();
 getCrypto();
+
+cryptoDiv.addEventListener('mouseover', () => {
+	cryptoDiv.classList.replace('unfocused-backdrop', 'focused-backdrop');
+});
+cryptoDiv.addEventListener('mouseleave', () => {
+	cryptoDiv.classList.replace('focused-backdrop', 'unfocused-backdrop');
+});
 
 async function getBackgroundPic() {
 	const url = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature";
@@ -42,7 +50,8 @@ async function getCrypto() {
 			throw new Error("It's an error!");
 		}
 		const data = await response.json();
-		insertFetchedCrypto(data);
+		const cryptoData = createCryptoDataItem(data);
+		insertFetchedCrypto(cryptoData);
 	}
 	catch {
 		console.error("something went wrong. no crypto for you!");
@@ -50,10 +59,33 @@ async function getCrypto() {
 	}
 }
 
+function createCryptoDataItem(data) {
+	return {
+		name: data.name,
+		image: data.image.small,
+		alt: `${data.name} icon`,
+		currentPrice: data.market_data.current_price.usd,
+		highPrice: data.market_data.high_24h.usd,
+		lowPrice: data.market_data.low_24h.usd
+	};
+}
+
 function insertFetchedCrypto(data) {
-	cryptoDiv.innerHTML = `
-		<img alt="crypto icon" class="crypto-icon" src="${data.image.small}" />
-		<p>${data.id}</p>`;
+	const cryptoTop = document.createElement('div');
+	cryptoTop.id = 'crypto-top';
+	cryptoTop.innerHTML = `
+		<img alt="${data.alt}" class="crypto-icon" src="${data.image}" />
+		<p>${data.name}</p>`;
+
+	const cryptoValues = document.createElement('ul');
+	cryptoValues.classList.add('crypto-values');
+	cryptoValues.innerHTML = `
+		<li>Current: $${data.currentPrice}</li>
+		<li>24-hour high: $${data.highPrice}</li>
+		<li>24-hour low: $${data.lowPrice}</li>`;
+
+	cryptoDiv.appendChild(cryptoTop);
+	cryptoDiv.appendChild(cryptoValues);
 }
 
 function insertCryptoNotAvailable() {
