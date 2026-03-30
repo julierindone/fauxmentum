2b39de88ea247e73fa17ee395425c7190c6715b6
Julie Rindone <julie.rindone@gmail.com>
2026-03-29 21:54:40 -0700

    Added fetchingcrypto functions & css

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const photographer = document.getElementById('author');
+const cryptoDiv = document.getElementById('crypto');
+
 getBackgroundPic();
+getCrypto();
 
 async function getBackgroundPic() {
 	const url = "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature";
@@ -30,7 +33,29 @@ function insertDefaultBackground() {
 	photographer.innerText = `"We'd love to have you for dinner..."`;
 }
 
-async function insertBackgroundPic() {
-	const data = await fetchBackground();
-	(document.getElementsByTagName("BODY"))[0].style.backgroundImage = `url(${data.urls.raw})`;
+async function getCrypto() {
+	const url = `https://api.coingecko.com/api/v3/coins/dogecoin`;
+
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error("It's an error!");
+		}
+		const data = await response.json();
+		insertFetchedCrypto(data);
+	}
+	catch {
+		console.error("something went wrong. no crypto for you!");
+		insertCryptoNotAvailable();
+	}
+}
+
+function insertFetchedCrypto(data) {
+	cryptoDiv.innerHTML = `
+		<img alt="crypto icon" class="crypto-icon" src="${data.image.small}" />
+		<p>${data.id}</p>`;
+}
+
+function insertCryptoNotAvailable() {
+	cryptoDiv.innerHTML = `<p>Data not available.</p>`;
 }
